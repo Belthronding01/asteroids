34783d614d242bde2abf8129d17a5b6468009f27
Belthronding01 <denizakin113@gmail.com>
2025-01-15 14:55:03 +0300

    Completed the base of the game, added aspects such as moving, shooting, colliding and splitting

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
 import pygame
-import constants
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
-	print("Starting asteroids!")
-	SCREEN_WIDTH = constants.SCREEN_WIDTH
-	SCREEN_HEIGHT = constants.SCREEN_HEIGHT
-	print(f"Screen width: {constants.SCREEN_WIDTH}")
-	print(f"Screen height: {constants.SCREEN_HEIGHT}")
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	asteroid_field = AsteroidField()
+
+	Shot.containers = (shots, updatable, drawable)
+	
+	Player.containers = (updatable, drawable)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	
 	while True:
 		for	event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		
-		screen.fill("black")
+		for obj in updatable:
+			obj.update(dt)
+			
 		
+		screen.fill("black")
+
+		for obj in drawable:
+			obj.draw(screen)
+
+		for asteroid in asteroids:
+			if asteroid.detect_collision(player):
+				raise SystemExit("Game Over")
+			for bullet in shots:
+				if asteroid.detect_collision(bullet):
+					asteroid.split()
+					bullet.kill()
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+
 	
 
 if __name__ == "__main__":
